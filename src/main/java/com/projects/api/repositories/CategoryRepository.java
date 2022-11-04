ca4b681926adfed5e31442f414a3da5775b7a049
Caio André <caioserralvo182@gmail.com>
2022-11-04 01:49:57 -0300

    feat: Category /Services/Resources/Repositories

---
+++ src/main/java/com/projects/api/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.projects.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projects.api.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
