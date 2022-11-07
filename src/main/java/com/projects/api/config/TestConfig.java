8bf820f7f5a087bcc60de8babe1cc9d8351e3652
Caio André <caioserralvo182@gmail.com>
2022-11-07 04:03:18 -0300

    feat: Payment

--- src/main/java/com/projects/api/config/TestConfig.java
+++ src/main/java/com/projects/api/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projects.api.entities.Category;
 import com.projects.api.entities.Order;
 import com.projects.api.entities.OrderItem;
+import com.projects.api.entities.Payment;
 import com.projects.api.entities.Product;
 import com.projects.api.entities.User;
 import com.projects.api.entities.enums.OrderStatus;
@@ -84,6 +85,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
